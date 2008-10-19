2734d750ee03c6ade10a89516d4f86fa384b3185
Alexander Lippling <lippling@gmail.com>
2008-10-19 22:19:02 +0000

    ProjectManager: Verzeichnisse werden nun anders durchsucht. Vorher 10 Minuten, jetzt 5 Sekunden!
    git-svn-id: svn://localhost/Projekte/DotNet/ProjectManager@214 3c14bec7-9347-e845-8214-f8cab9b17e37

--- ProjectManager/SolutionFileCollection.cs
+++ ProjectManager/SolutionFileCollection.cs
@@ -16,10 +16,56 @@ namespace ProjectManager
             if (!dir.Exists)
                 throw new ArgumentException("Path does not exist!");
 
-            foreach (var file in dir.GetFiles("*.sln", SearchOption.AllDirectories))
+            foreach (var file in GetAllFileNames(rootPath))
             {
-                Add(new SolutionFile { FullName = file.FullName });
+                Add(new SolutionFile { FullName = file });
             }
+        }
+
+        /// <summary>
+        /// Find all files in a directory, and all files within every nested
+        /// directory.
+        /// </summary>
+        /// <param name="baseDir">The starting directory you want to use.</param>
+        /// <returns>A string array containing all the file names.</returns>
+        private static IList<string> GetAllFileNames(string baseDir)
+        {
+            //
+            // Store results in the file results list.
+            //
+            List<string> fileResults = new List<string>();
+
+            //
+            // Store a stack of our directories.
+            //
+            Stack<string> directoryStack = new Stack<string>();
+            directoryStack.Push(baseDir);
+
+            //
+            // While there are directories to process
+            //
+            while (directoryStack.Count > 0)
+            {
+                string currentDir = directoryStack.Pop();
+
+                //
+                // Add all files at this directory.
+                //
+                foreach (string fileName in Directory.GetFiles(currentDir, "*.sln"))
+                {
+                    fileResults.Add(fileName);
+                }
+
+                //
+                // Add all directories at this directory.
+                //
+                foreach (string directoryName in Directory.GetDirectories(currentDir))
+                {
+                    if (!directoryName.Contains("\\.svn"))
+                        directoryStack.Push(directoryName);
+                }
+            }
+            return fileResults;
         }
     }
 }
